467b4fa8ce6bf968e5e0cf12d47595db9a00b9b2
casperwang <casperwang2014@gmail.com>
2023-08-26 20:43:03 +0800

    revision of codebook

--- codes/Basic/debug.cpp
+++ codes/Basic/debug.cpp
@@ -1,22 +1,11 @@
-#ifdef CKISEKI
+#ifdef too_soft
 #define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
-#define debug(a...) debug_(#a, a)
-#define orange(a...) orange_(#a, a)
-template <typename ...T>
-void debug_(const char *s, T ...a) {
-    cerr << "\e[1;32m(" << s << ") = (";
-    int cnt = sizeof...(T);
-    (..., (cerr << a << (--cnt ? ", " : ")\e[0m\n")));
-}
-template <typename I>
-void orange_(const char *s, I L, I R) {
-    cerr << "\e[1;32m[ " << s << " ] = [ ";
-    for (int f = 0; L != R; ++L)
-        cerr << (f++ ? ", " : "") << *L;
-    cerr << " ]\e[0m\n";
-}
+#define debug(args...) kout("\e[1;32m[ " + string (#args) + " ]\e[0m", args)
+void kout() { cerr << endl; }
+template <class T, class ...U> void kout(T a, U ...b) { cerr << a << ' ',kout(b...); }
+template <class T> void pary(T L, T R) { while (L != R) cerr << *L << " \n"[++L==R]; }
 #else
 #define safe ((void)0)
 #define debug(...) safe
-#define orange(...) safe
+#define pary(...) safe
 #endif
