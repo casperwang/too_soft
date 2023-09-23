231112c97359e1b418cf22b17e28d6ed53a6a0e7
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-09-23 22:01:58 +0800

    Add short SA

---
+++ codes/String/Suffix_Array_Short.cpp
@@ -0,0 +1,33 @@
+struct SA {
+  int n; vector<int> sa, rk, tmp, c, lcp;
+  SA(auto &v, int z = 256) : n(v.size()), sa(n),
+    rk(all(v)), tmp(n), lcp(n) {
+    for (int i = 0; i < n; i++) rk.push_back(-1);
+    const auto radix = [&](int s) {
+      vector<int> c(z + 1);
+      for (int i = 0; i < n; i++) ++c[rk[i + s] + 1];
+      for (int i = 1; i <= z; i++) c[i] += c[i - 1];
+      for (int i = n - 1; i >= 0; i--)
+        tmp[--c[rk[sa[i] + s] + 1]] = sa[i];
+      sa = tmp;
+    };
+    iota(sa.begin(), sa.end(), 0);
+    for (int L = 1; L < n; L *= 2) {
+      for (int s: {L, 0}) radix(s);
+      const auto F = [&](int a) {
+        return pair(rk[a], rk[a + L]); };
+      tmp[0] = 0;
+      for (int i = 1; i < n; i++)
+        tmp[i] = tmp[i - 1] + (F(sa[i - 1]) < F(sa[i]));
+      for (int i = 0; i < n; i++) rk[sa[i]] = tmp[i];
+      z = tmp[n - 1] + 1;
+    }
+    for (int i = 0, h = 0; i < n; i++) if (rk[i]) {
+      if (h > 0) --h;
+      int j = sa[rk[i] - 1];
+      while (i + h < n && j + h < n
+          && v[i + h] == v[j + h]) ++h;
+      lcp[rk[i] - 1] = h;
+    }
+  }
+};
